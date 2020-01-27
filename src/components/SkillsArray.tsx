12d3405f6a5b254ea58b9e9ed502ddaa96652ccb
Nicolas JOULIN <njoulin@woodbrass.com>
2020-01-27 10:00:12 +0100

    changed container for skills array

--- src/components/SkillsArray.tsx
+++ src/components/SkillsArray.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { Card, CardContent, Grid, Typography, Container } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -10,11 +10,11 @@ const useStyles = makeStyles((theme: Theme) =>
       marginTop: theme.spacing(2),
       margin: theme.spacing(8)
     },
-    skillText: {
-      marginLeft: theme.spacing(2)
+    skillImage: {
+      margin: theme.spacing(1)
     },
-    skillItem: {
-      marginLeft: theme.spacing(8)
+    skillText: {
+      marginLeft:theme.spacing(2)
     }
   }),
 );
@@ -25,15 +25,14 @@ function SkillsArray() {
   const skills = ['php', 'javascript', 'sql', 'css', 'docker', 'react', 'mongoDB', 'typescript', 'nodejs', 'git', 'jenkins', 'nginx'];
 
   return(
-    <Grid container spacing={2} direction="row" justify="center" alignItems="center">
-      <Grid item sm={10}>
-        <Card elevation={20} className={classes.skillsCard}>
+    <Container maxWidth="md">
+      <Card elevation={20} className={classes.skillsCard}>
           <CardContent>
-            <Grid container spacing={2}>
+            <Grid container>
               {skills.map(skill => (
                 <Grid key={skill} item sm={6} xs={12}>
                   <Grid container direction="row" justify="flex-start" alignItems="center">
-                    <img src={"/static/images/skills/"+skill+".png"} alt={skill} height="50" width="50"></img>
+                    <img src={"/static/images/skills/"+skill+".png"} alt={skill} height="50" width="50" className={classes.skillImage}></img>
                     <Typography className={classes.skillText}>
                       {skill}
                     </Typography>
@@ -43,8 +42,7 @@ function SkillsArray() {
             </Grid>
           </CardContent>
         </Card>
-      </Grid>
-    </Grid>
+    </Container>
   );
 }
 
