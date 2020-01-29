fdf66fdafd4eadcbe1dd97f69e4d8533f503eff1
Nicolas JOULIN <njoulin@woodbrass.com>
2020-01-29 09:40:30 +0100

    added skill tooltip

--- src/components/SkillsArray.tsx
+++ src/components/SkillsArray.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Card, CardContent, Grid, Typography, Container } from '@material-ui/core';
+import { Card, CardContent, Grid, Container } from '@material-ui/core';
+
+import SkillItem from './SkillItem';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,22 +23,19 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function SkillsArray() {
   const classes = useStyles();
-
   const skills = ['php', 'javascript', 'sql', 'css', 'docker', 'react', 'mongoDB', 'typescript', 'nodejs', 'git', 'jenkins', 'nginx'];
 
   return(
     <Container maxWidth="md">
       <Card elevation={8} className={classes.skillsCard}>
           <CardContent>
-            <Grid container>
+            <Grid container
+            direction="row"
+            justify="space-around"
+            alignItems="center">
               {skills.map(skill => (
-                <Grid key={skill} item sm={6} xs={12}>
-                  <Grid container direction="row" justify="flex-start" alignItems="center">
-                    <img src={"/static/images/skills/"+skill+".png"} alt={skill} height="50" width="50" className={classes.skillImage}></img>
-                    <Typography className={classes.skillText}>
-                      {skill}
-                    </Typography>
-                  </Grid>
+                <Grid key={skill} item xs={4} sm={6}>
+                  <SkillItem icon={"/static/images/skills/"+skill+".png"} tooltip={skill} />
                 </Grid>
               ))}
             </Grid>
